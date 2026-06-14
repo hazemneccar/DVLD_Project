0da7c483b2944f90cc3f387e163c4904ca670f4b
Hazem NECCAR <hazmalnajar@gmail.com>
2026-06-14 18:25:00 +0300

    Added ApplicationType Data access Layer

---
+++ DVLD_DataAccess/clsApplicationTypeData.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsApplicationTypeData
+    {
+        public static bool GetAppTypeInfoByID(int applicationTypeID, ref string applicationTypeTitle, ref float applicationFees)
+        {
+            bool isFound = false;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+            string query = "SELECT * FROM ApplicationTypes WHERE ApplicationTypeID = @ApplicationTypeID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ApplicationTypeID", applicationTypeID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader Reader = command.ExecuteReader();
+
+                if (Reader.Read())
+                {
+                    applicationTypeTitle = (string)Reader["ApplicationTypeTitle"];
+                    applicationFees = (float)Reader["ApplicationFees"];
+
+                    isFound = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+        public static bool UpdateAppTypeInfo(int applicationID, string applicationTitle, float applicationFees)
+        {
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+
+            string query = @"UPDATE ApplicationTypes 
+                     SET ApplicationTypeTitle = @ApplicationTitle,
+                         ApplicationFees = @ApplicationFees
+                     WHERE ApplicationTypeID = @ApplicationID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ApplicationID", applicationID);
+            command.Parameters.AddWithValue("@ApplicationTitle", applicationTitle);
+            command.Parameters.AddWithValue("@ApplicationFees", applicationFees);
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
+        public static DataTable GetAllApplicationTypes()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+            string query = "SELECT * FROM ApplicationTypes order by ApplicationTypeTitle";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                    dt.Load(reader);
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+        }
+    }
+}
