6ae8f856dd87869cc4298ff8ac9f3e5a900da726
Hazem NECCAR <hazmalnajar@gmail.com>
2026-07-17 14:30:00 +0300

    Added TestTypes info

---
+++ DVLD_DataAccess/clsTestTypeData.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    public class clsTestTypeData
+    {
+        public static bool GetTestTypeInfoByID(int testTypeID, ref string testTypeTitle, ref string testTypeDescription, ref float testTypeFees)
+        {
+            bool isFound = false;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+            string query = "SELECT * FROM TestTypes WHERE TestTypeID = @TestTypeID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@TestTypeID", testTypeID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader Reader = command.ExecuteReader();
+
+                if (Reader.Read())
+                {
+                    testTypeTitle = (string)Reader["TestTypeTitle"];
+                    testTypeDescription = (string)Reader["TestTypeDescription"];
+                    testTypeFees = (float)Reader["TestTypeFees"];
+
+                    isFound = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+        public static DataTable GetAllTestTypes()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+            string query = "SELECT * FROM TestTypes";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                    dt.Load(reader);
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dt;
+        }
+        public static bool UpdateTestType(int testTypeID, string testTypeTitle, string testTypeDescription, float testTypeFees)
+        {
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString);
+
+            string query = @"UPDATE TestTypes 
+                     SET TestTypeTitle = @TestTypeTitle,
+                         TestTypeDescription = @TestTypeDescription,
+                         TestTypeFees = @TestTypeFees
+                     WHERE TestTypeID = @TestTypeID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestTypeID", testTypeID);
+            command.Parameters.AddWithValue("@TestTypeTitle", testTypeTitle);
+            command.Parameters.AddWithValue("@TestTypeDescription", testTypeDescription);
+            command.Parameters.AddWithValue("@TestTypeFees", testTypeFees);
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
+    }
+}
